43963416f56041f7e2c955641c7a26450b31ccac
Jamie Symonds <jamie.symonds@gmail.com>
2022-04-01 12:37:40 -0300

    added edit func, added please_wait

--- src/main.js
+++ src/main.js
@@ -6,6 +6,9 @@ import store from './store'
 
 import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
 
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
+
 Vue.config.productionTip = false
 
 new Vue({
--- src/views/CustomerList.vue
+++ src/views/CustomerList.vue
@@ -18,7 +18,10 @@
 
     <br/>
 
+
     <button @click="showHideForm()" v-if="!show_add_new_form">Show Add New Form</button>
+
+    <h1 v-if="please_wait" style="color: red;">PLEASE WAIT...</h1>
 
     <br/>
     <br/>
@@ -26,8 +29,40 @@
     <table class="table table-border table-striped">
       <tr><th>first</th><th>last</th><th>email</th></tr>
       <tr v-for="(customer, index) in customers" v-bind:key="customer._id">
-        <td>{{ customer.first_name  }}</td><td>{{ customer.last_name }}</td><td> {{ customer.email }}</td>
-        <td><button @click="deleteCustomer(customer, index)">DELETE</button></td>
+        <td>
+          <span v-if="editing_record == customer._id">
+            <input type="text" v-model="customer.first_name"/>
+          </span>
+          <span v-else>
+            {{ customer.first_name  }}
+          </span>
+        </td>
+        <td>
+          <span v-if="editing_record == customer._id">
+            <input type="text" v-model="customer.last_name"/>
+          </span>
+          <span v-else>
+            {{ customer.last_name  }}
+          </span>
+        </td>
+        <td>
+          <span v-if="editing_record == customer._id">
+            <input type="text" v-model="customer.email"/>
+          </span>
+          <span v-else>
+            {{ customer.email  }}
+          </span>
+        </td>
+        <td>
+          <span v-if="editing_record == customer._id">
+            <button @click="saveCustomer(customer)">SAVE</button>
+            <button @click="cancelCustomer()">CANCEL</button>
+          </span>
+          <span v-else>
+            <button @click="deleteCustomer(customer, index)">DELETE</button>
+            <button @click="editCustomer(customer)">EDIT</button>
+          </span>
+        </td>
       </tr>
     </table>
 
@@ -42,6 +77,8 @@ export default {
   name: "CustomerList",
   data() {
     return {
+      please_wait: false,
+      editing_record: 0,
       show_add_new_form: false,
       customers: [],
       new_customer: {
@@ -52,17 +89,65 @@ export default {
     }
   },
   methods: {
+    editCustomer: function(customer) {
+      console.log(customer);
+      //alert("you clicked edit");
+      this.editing_record = customer._id;
+
+    },
+    saveCustomer: function(customer) {
+      console.log(customer);
+
+      console.log("saving customer " + customer._id);
+
+      let cust = this.customers.filter(c => c._id == customer._id);
+
+      let data = { customer: cust[0] };
+
+      var url = "http://localhost:8080/api/customers/";
+
+      this.please_wait = true;
+
+      axios.put(url, data)
+      .then((response) => {
+        //alert("saved!");
+        this.please_wait = false;
+        console.log(response);
+        this.editing_record = 0;
+      }, (error) => {
+        this.please_wait = false;
+        console.log(error);
+        alert("Error updating customer.");
+      });
+
+
+    },
+    cancelCustomer: function() {
+      this.editing_record = 0;
+    },
     showHideForm: function() {
       this.show_add_new_form = !this.show_add_new_form;
     },
     getCustomers: function() {
+      this.please_wait = true;
+
       var url = "http://localhost:8080/api/customers/";
       axios.get(url, {})
         .then((response) => {
+          this.please_wait = false;
           this.customers = response.data;
         })
     },
     createCustomer: function() {
+
+      if (this.new_customer.last_name.length > 100 || this.new_customer.first_name.length > 100
+      || this.new_customer.email.indexOf("@") == -1) {
+        alert("you did something dumb");
+        return;
+      }
+
+      this.please_wait = true;
+
       var url = "http://localhost:8080/api/customers/";
       axios.post(url, {
           first_name: this.new_customer.first_name,
@@ -78,21 +163,26 @@ export default {
         this.new_customer.last_name = "";
         this.new_customer.email = "";
         this.show_add_new_form = false;
+        this.please_wait = false;
       }, (error) => {
+        this.please_wait = false;
         alert("Error adding new customer");
         console.log(error);
       });
     },
     deleteCustomer: function(customer, index) {
+      this.please_wait = true;
       var url = "http://localhost:8080/api/customers/" + customer._id;
       axios.delete(url, {
         headers: {},
         data: {}
       })
       .then((response) => {
+        this.please_wait = false;
         console.log(response);
         this.$delete(this.customers, index);
       }, (error) => {
+        this.please_wait = false;
         console.log(error);
       });
     }
